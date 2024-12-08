0bfeb60900841e856c749aa26c5a6d4cf211ac6b
4b3g0105@office.stust.edu.tw <4b3g0105@office.stust.edu.tw>
2024-12-08 18:02:09 +0800

    新增 Student 類別及相關檔案，調整現有程式碼
    在 `practise1.cpp` 中：
      * 調整了 `#include` 的順序，新增了 `#include "Student.h"` 和 `#include "Utility.h"`。
      * 修改了 `person1` 的出生日期。
      * 將 `person2` 由物件改為指標，並使用 `new` 來動態分配記憶體。
      * 修改了 `person2` 的顯示方式，改用指標呼叫 `display` 方法。
      * 新增了一個 `Student` 物件 `student1`，並呼叫其 `display` 方法。
    
    在 `practise1.vcxproj` 中：
      * 新增了 `Student.cpp`、`Teacher.cpp` 和 `Utility.cpp` 到 `ClCompile` 節點。
      * 新增了 `Student.h`、`Teacher.h` 和 `Utility.h` 到 `ClInclude` 節點。
    
    在 `practise1.vcxproj.filters` 中：
      * 新增了 `Student.cpp`、`Teacher.cpp` 和 `Utility.cpp` 到 `ClCompile` 節點，並設置了 `來源檔案` 過濾器。
      * 新增了 `Student.h`、`Teacher.h` 和 `Utility.h` 到 `ClInclude` 節點，並設置了 `標頭檔` 過濾器。
    
    在 `Student.cpp` 中：
      * 新增了 `Student` 類別的實作，包括建構子、getter 和 setter 方法，以及 `display` 方法。
    
    在 `Student.h` 中：
      * 新增了 `Student` 類別的定義，繼承自 `Person` 類別，並包含 `studentId`、`department` 和 `className` 成員變數，以及相應的 getter 和 setter 方法。
    
    在 `Teacher.cpp` 中：
      * 新增了 `#include "Teacher.h"`。
    
    在 `Teacher.h` 中：
      * 新增了 `Teacher` 類別的定義，繼承自 `Person` 類別。
    
    在 `Utility.cpp` 中：
      * 新增了 `Utility` 類別的實作，包括將 `Department` 和 `ClassName` 枚舉轉換為字串的方法。
    
    在 `Utility.h` 中：
      * 新增了 `Utility` 類別的定義，包含將 `Department` 和 `ClassName` 枚舉轉換為字串的靜態方法。
      * 定義了 `Department` 和 `ClassName` 枚舉。

--- practise1/practise1.cpp
+++ practise1/practise1.cpp
@@ -1,21 +1,25 @@
-﻿#include <iostream>
-#include <string>
+﻿#include <string>
+#include <iostream>
 #include "Person.h"
+#include "Student.h"
+#include "Utility.h"
 using namespace std;
 
 int main()
 {
-	Person person1("A123456789", "Chen", "Jason", "M", "1990-01-01");
+	Person person1("A123456789", "Chen", "Jason", "M", "1999-01-01");
 
-	Person person2;
-	person2.setId("B987654321");
-	person2.setLastName("Wang");
-	person2.setFirstName("David");
-	person2.setGender("M");
-	person2.setBirthDate("1999-12-31");
+	Person* person2 = new Person();
+	person2->setId("B987654321");
+	person2->setLastName("Wang");
+	person2->setFirstName("David");
+	person2->setGender("M");
+	person2->setBirthDate("1999-12-31");
 
 	person1.display();
 	cout << endl;
-	person2.display();
+	person2->display();
 
+	Student student1("A123456789", "Chen", "Jason", "M", "1999-01-01", "S001", Department::ComputerSciece, ClassName::_1A);
+	student1.display();
 }
